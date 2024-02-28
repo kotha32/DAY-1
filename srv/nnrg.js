ea3bb3cbf4cfcceeee9d6a25aa2c16930e4b78ac
kotha32 <sumanthkotha32@gmail.com>
2024-02-28 17:31:45 +0530

    made changes in js

--- srv/nnrg.js
+++ srv/nnrg.js
@@ -2,15 +2,6 @@ const cds = require('@sap/cds');
 module.exports = cds.service.impl(function () {
     const { BusinessPartners, States, Products } = this.entities();
     this.before(["CREATE"], BusinessPartners, async (req) => {
-        // const gst = req.data.gst_no;
-        // const is_gstn_registered = req.data.is_gstn_registered;
-        // if (is_gstn_registered == true && gst == null) {
-        //   req.error({
-        //     code: "INVALID_GST_NO",
-        //     message: "Enter gst registered number please",
-        //     target: "is_gstn_registered",
-        //   });
-        // }
         const results = await cds
         .transaction(req)
         .run(SELECT.from(BusinessPartners));
